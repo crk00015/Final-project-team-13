1bdfce65df025f5548b34de59c6c3787a3a1df87
crk00015 <crk00015@mix.wvu.edu>
2025-04-22 20:21:47 -0400

    question 1 done

---
+++ video_game_sales.py
@@ -0,0 +1,38 @@
+import marimo
+
+__generated_with = "0.10.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    import plotly.express as px
+    return mo, pl, px
+
+
+@app.cell
+def _(pl):
+    data = pl.read_csv("vgchartz-2024.csv")
+    return (data,)
+
+
+@app.cell
+def _(data, pl, px):
+    #Sold most titles
+    most_sold = data.group_by("title").agg([
+        pl.col("total_sales").sum().round(2).alias("Worldwide_sales")
+    ]).sort("Worldwide_sales",descending=True)
+
+    shorten = most_sold.head(15)
+
+    most_sold_chart = px.bar(shorten,x= "title", y="Worldwide_sales", title = "Worldwide Sales of the Top 15 Game Titles")
+    most_sold
+    most_sold_chart
+
+    return most_sold, most_sold_chart, shorten
+
+
+if __name__ == "__main__":
+    app.run()
