080fbcb93859ab7d1eb7fd90d42673c7c2bfa738
mcm00043 <mcm00043@mix.wvu.edu>
2025-04-24 16:02:23 -0400

    Update best_release_month.py

--- best_release_month.py
+++ best_release_month.py
@@ -14,40 +14,38 @@ def _():
 
 app._unparsable_cell(
     r"""
-    What month release data has the best sales worlwide
+    What video game month release has the best sales worlwide
     what is north americas best month, japan , and africas
-    compare to japan month release
+    compare to japan month release.
     """,
     name="_"
 )
 
 
-app._unparsable_cell(
-    r"""
-    data1 = pl.read_parquet(\"vgchartz-2024.parquet\").with_columns( 
-                pl.col(\"release_date\").dt.year().alias(\"year\"),
-                pl.col(\"release_date\").dt.month().alias(\"month\"),
-                pl.col(\"release_date\").dt.day().alias(\"day\"),
-    )
-    .with_columns((pl.when(pl.col(\"month\") == 1).then(pl.lit(\"January\"))
-                .when(pl.col(\"month\") == 2).then(pl.lit(\"February\"))
-                .when(pl.col(\"month\") == 3).then(pl.lit(\"March\"))
-                .when(pl.col(\"month\") == 4).then(pl.lit(\"April\"))
-                .when(pl.col(\"month\") == 5).then(pl.lit(\"May\"))
-                .when(pl.col(\"month\") == 6).then(pl.lit(\"June\"))
-                .when(pl.col(\"month\") == 7).then(pl.lit(\"July\"))
-                .when(pl.col(\"month\") == 8).then(pl.lit(\"August\"))
-                .when(pl.col(\"month\") == 9).then(pl.lit(\"September\"))
-                .when(pl.col(\"month\") == 10).then(pl.lit(\"October\"))
-                .when(pl.col(\"month\") == 11).then(pl.lit(\"November\"))
-                .when(pl.col(\"month\") == 12).then(pl.lit(\"December\"))
-                .otherwise(pl.lit(\"Invalid month\"))
-                .alias(\"month\")
-    ))
+@app.cell
+def _(pl):
+    data1 = pl.read_parquet("vgchartz-2024.parquet").with_columns( 
+            pl.col("release_date").dt.year().alias("year"),
+            pl.col("release_date").dt.month().alias("month"),
+            pl.col("release_date").dt.day().alias("day"),
+        ).with_columns(
+            pl.when(pl.col("month") == 1).then(pl.lit("January"))
+            .when(pl.col("month") == 2).then(pl.lit("February"))
+            .when(pl.col("month") == 3).then(pl.lit("March"))
+            .when(pl.col("month") == 4).then(pl.lit("April"))
+            .when(pl.col("month") == 5).then(pl.lit("May"))
+            .when(pl.col("month") == 6).then(pl.lit("June"))
+            .when(pl.col("month") == 7).then(pl.lit("July"))
+            .when(pl.col("month") == 8).then(pl.lit("August"))
+            .when(pl.col("month") == 9).then(pl.lit("September"))
+            .when(pl.col("month") == 10).then(pl.lit("October"))
+            .when(pl.col("month") == 11).then(pl.lit("November"))
+            .when(pl.col("month") == 12).then(pl.lit("December"))
+            .otherwise(pl.lit("Invalid month"))
+            .alias("month_name")
+        )
     data1
-    """,
-    name="_"
-)
+    return (data1,)
 
 
 @app.cell
@@ -59,13 +57,13 @@ def _(pl):
 
 @app.cell
 def _(data1, pl):
-    best_year = data1.group_by("month").agg([
+    best_monthww = data1.group_by("month").agg([
         pl.col("total_sales").sum().round(2).alias("Worldwide_sales")
     ]).sort("Worldwide_sales",descending=True)
 
 
-    best_year
-    return (best_year,)
+    best_monthww
+    return (best_monthww,)
 
 
 @app.cell
